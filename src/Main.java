8d1eb8d9e8f732e7ecc635d8b8496499bf54cab5
Thiago <thiagoksc20@gmail.com>
2025-09-15 23:16:29 -0300

    .\src\

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+          Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        String nome = sc.next();
 
         }
 
