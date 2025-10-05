d26fa6b951eb095ca7220a3b25d2aa2cd1ae6f31
Thiago <thiagoksc20@gmail.com>
2025-10-05 19:36:46 -0300

    pacienteEspecial atualizado

--- src/com/hospitalar/sistema/PacienteEspecial.java
+++ src/com/hospitalar/sistema/PacienteEspecial.java
@@ -1,18 +1,21 @@
 package com.hospitalar.sistema;
 
 public class PacienteEspecial extends Paciente {
-    private String planoDeSaude;
+    private PlanoDeSaude planoDeSaude;
 
-    public PacienteEspecial(String nome, String cpf, int idade, String planoDeSaude) {
+    public PacienteEspecial(String nome, String cpf, int idade, PlanoDeSaude planoDeSaude) {
         super(nome, cpf, idade);
         this.planoDeSaude = planoDeSaude;
     }
-    public String getPlanoDeSaude() {
+
+    public PlanoDeSaude getPlanoDeSaude() {
         return planoDeSaude;
     }
+
     @Override
-    public void exibirInformacoes(){
+    public void exibirInformacoes() {
         super.exibirInformacoes();
-        System.out.println("Plano de saude: " + planoDeSaude);
+        System.out.println("Plano de Saude: " + planoDeSaude.getNome());
     }
+
 }
