3a2f17f37bcda77aa252ea41bfcd0ef62ad471dd
Thiago <thiagoksc20@gmail.com>
2025-10-05 14:35:34 -0300

    internação atualizado

--- src/com/hospitalar/sistema/Internacao.java
+++ src/com/hospitalar/sistema/Internacao.java
@@ -1,22 +1,91 @@
 package com.hospitalar.sistema;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Internacao {
+    private Paciente paciente;
+    private Medico medicoResponsavel;
     private LocalDate dataEntrada;
     private LocalDate dataSaida;
-    private String motivo;
+    private String quarto;
+    private double custoDiaria;
+    private StatusInternacao status;
 
-    public Internacao(LocalDate dataEntrada, String motivo) {
+    public Internacao(Paciente paciente, Medico medicoResponsavel, String quarto, double custoDiaria) {
+        this.paciente = paciente;
+        this.medicoResponsavel = medicoResponsavel;
+        this.quarto = quarto;
+        this.custoDiaria = custoDiaria;
+        this.dataEntrada = LocalDate.now();
+        this.dataSaida = null;
+        this.status = StatusInternacao.ATIVA;
+    }
+
+    public Internacao(Paciente paciente, Medico medico, LocalDate dataEntrada, String quarto, double custoDiaria, StatusInternacao status) {
+        this.paciente = paciente;
+        this.medicoResponsavel = medico;
+        this.quarto = quarto;
+        this.custoDiaria = custoDiaria;
         this.dataEntrada = dataEntrada;
         this.dataSaida = dataSaida;
-        this.motivo = null;
+        this.status = status;
     }
-    public void setDataSaida(LocalDate dataSaida){
-        this.dataSaida = dataSaida;
+
+    public double registrarAlta() {
+        if (this.status == StatusInternacao.ATIVA) {
+            this.dataSaida = LocalDate.now();
+            this.status = StatusInternacao.FINALIZADA;
+            long diasInternado = ChronoUnit.DAYS.between(dataEntrada, dataSaida);
+            diasInternado = diasInternado == 0 ? 1 : diasInternado;
+            return diasInternado * this.custoDiaria;
+        }
+        return 0;
     }
-    @Override
-    public String toString(){
-        return "Internacao [Entrada =" + dataEntrada + ", Saida=" + (dataSaida == null ? "Pendente" : dataSaida) + ", Motivo=" + motivo + "]";
+
+    public void cancelar() {
+        this.status = StatusInternacao.CANCELADA;
     }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public Medico getMedicoResponsavel() {
+        return medicoResponsavel;
+    }
+
+    public LocalDate getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public LocalDate getDataSaida() {
+        return dataSaida;
+    }
+
+    public String getQuarto() {
+        return quarto;
+    }
+
+    public double getCustoDiaria() {
+        return custoDiaria;
+    }
+
+    public StatusInternacao getStatus() {
+        return status;
+    }
+
+    public void exibirInformacoes() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println("Paciente: " + paciente.getNome() + " (CPF: " + paciente.getCpf() + ")");
+        System.out.println("Médico Resp.: Dr(a). " + medicoResponsavel.getNome());
+        System.out.println("Quarto: " + quarto);
+        System.out.println("Status: " + status);
+        System.out.println("Data de Entrada: " + dataEntrada.format(formatter));
+        if (dataSaida != null) {
+            System.out.println("Data de Saída: " + dataSaida.format(formatter));
+        }
+    }
+
 }
---
+++ src/com/hospitalar/sistema/StatusInternacao.java
@@ -0,0 +1,7 @@
+package com.hospitalar.sistema;
+
+public enum StatusInternacao {
+    ATIVA,
+    FINALIZADA,
+    CANCELADA;
+}
